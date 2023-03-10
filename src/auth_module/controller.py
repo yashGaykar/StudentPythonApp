1d5cd8b9183dc8a7b4d68c2258a3e7fcedaecf82
yashGaykar <yash.gaykar@opcito.com>
2023-03-10 10:57:40 +0530

    adds: schemas

--- src/auth_module/controller.py
+++ src/auth_module/controller.py
@@ -5,24 +5,31 @@ from settings import NODE_APP
 from http import HTTPStatus
 from time import time
 from .service import *
+from .schema import UserSchema
 
 
 def login():
     """User Login"""
 
     params = request.get_json()
+
     try:
+        user_schema = UserSchema()
+        errors = user_schema.validate(params)
+        if errors:
+            return {"errors": errors}, HTTPStatus.BAD_REQUEST
+
         # validation
-        validate_email(params["email"])
-        validate_password(params["password"])
+        # validate_email(params["email"])
+        # validate_password(params["password"])
 
-        #Login Service
-        response = AuthService.login(params["email"],params["password"])
+        # Login Service
+        response = AuthService.login(params["email"], params["password"])
 
-        return jsonify({"message":"Logged in Successfully"}), HTTPStatus.OK   
+        return jsonify({"message": "Logged in Successfully"}), HTTPStatus.OK
 
     except KeyError as e:
-        return jsonify({"Error":f'{e} is required'})
+        return jsonify({"Error": f'{e} is required'}), HTTPStatus.BAD_REQUEST
     except Exception as err:
         return jsonify({"Error": str(err)}), HTTPStatus.BAD_REQUEST
 
@@ -30,7 +37,6 @@ def login():
 def logout():
     """User Logout"""
 
-    
     if ('token' in session.keys()):
         session.pop("token")
         return jsonify({"message": "Successfully Logged Out"})
